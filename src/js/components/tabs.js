9a4d57f26bbc9f55760eb6ac68f2cfacce28caac
korchinmv <korchinmv@yandex.ru>
2024-12-08 00:03:06 +0300

    add: rpa section

--- src/js/components/tabs.js
+++ src/js/components/tabs.js
@@ -1,21 +1,42 @@
-const tabs = document.querySelectorAll(".tabs__nav-item");
-const contents = document.querySelectorAll(".tabs__panel");
+const productTabs = document.querySelectorAll(".tabs__nav-item");
+const productContents = document.querySelectorAll(".tabs__panel");
+const rpaTabs = document.querySelectorAll(".horizontal-tabs__nav-item");
+const rpaContents = document.querySelectorAll(".horizontal-tabs__panel");
+const activeTab = document.querySelector(".tabs__nav-item--active");
+const underline = document.querySelector(".horizontal-tabs__underline");
+function updateUnderline() {
+  const activeTab = document.querySelector(
+    ".horizontal-tabs__nav-item--active"
+  );
 
-tabs.forEach((tab) => {
-  tab.addEventListener("click", () => {
-    console.log("click");
+  if (underline) {
+    underline.style.maxWidth = `${activeTab.offsetWidth}px`;
+    underline.style.left = `${activeTab.offsetLeft}px`;
+  }
+}
+const changeActiveTab = (tabs, contents) => {
+  tabs.forEach((tab) => {
+    tab.addEventListener("click", () => {
+      tabs.forEach((t) =>
+        t.classList.remove("horizontal-tabs__nav-item--active")
+      );
+      tab.classList.add("horizontal-tabs__nav-item--active");
 
-    tabs.forEach((t) => t.classList.remove("tabs__nav-item--active"));
-    tab.classList.add("tabs__nav-item--active");
+      const target = tab.getAttribute("data-target");
 
-    const target = tab.getAttribute("data-target");
+      contents.forEach((content) => {
+        if (content.id === target) {
+          content.classList.add("horizontal-tabs__panel--active");
+        } else {
+          content.classList.remove("horizontal-tabs__panel--active");
+        }
+      });
 
-    contents.forEach((content) => {
-      if (content.id === target) {
-        content.classList.add("tabs__panel--active");
-      } else {
-        content.classList.remove("tabs__panel--active");
-      }
+      updateUnderline();
     });
   });
-});
+};
+
+changeActiveTab(productTabs, productContents);
+changeActiveTab(rpaTabs, rpaContents);
+updateUnderline();
