7c11c433e61438aa543d006782e972256bdb48b2
korchinmv <korchinmv@yandex.ru>
2024-12-09 11:28:23 +0300

    end

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -25,3 +25,57 @@ const servicesSwiper = new Swiper(".tasks__list-slider", {
     },
   },
 });
+
+const rpaPlatformSwiper = new Swiper(".horizontal-tabs__panel-swiper", {
+  slidesPerView: 1.3,
+  spaceBetween: 16,
+  breakpoints: {
+    // when window width is >= 1200px
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 24,
+    },
+  },
+});
+
+const decicionsSwiper = new Swiper(".decicions__slider", {
+  slidesPerView: 1,
+  spaceBetween: 24,
+  navigation: {
+    nextEl: ".decicions__slider-controls-next",
+    prevEl: ".decicions__slider-controls-prev",
+  },
+  pagination: {
+    el: ".decicions__pagination",
+    clickable: true,
+  },
+});
+
+const newsSwiper = new Swiper(".news__slider", {
+  slidesPerView: 1.3,
+  spaceBetween: 16,
+  loop: true,
+  navigation: {
+    nextEl: ".news__slider-controls-next",
+    prevEl: ".news__slider-controls-prev",
+  },
+  pagination: {
+    el: ".news__pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    // when window width is >= 1200px
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 24,
+    },
+  },
+});
